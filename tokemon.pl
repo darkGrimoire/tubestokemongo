9c1b5ca93e05459167b8d74d6eb963ee5fcd0a71
annisarahim <53399300+annisarahim@users.noreply.github.com>
2019-11-11 15:51:05 +0700

    nambah3

--- tokemon.pl
+++ tokemon.pl
@@ -1,55 +1,40 @@
-:-dynamic(tokemon/1).  /*tokemon(nama)*/
+:-dynamic(tokemon/6).  /*tokemon(nama)*/
 
-tokemon(catamon).
-tokemon(dogmon).
-tokemon(birdomon).
-tokemon(mousemon).
-tokemon(garamon).
-tokemon(daemon).
-tokemon(kumon).
-tokemon(doraemon).
+/*isTokemon(nama,jenis,tipe,hp,attack,sp_attack) */
+isTokemon(catamon,normal,water,500,50,70).
+isTokemon(anjingmon,normal,fire,450,60,80).
+isTokemon(birdmon,normal,leaves,400,70,90).
+isTokemon(tikusmon,normal,leaves,400,70,90).
+isTokemon(dragonmon,normal,fire,500,60,80).
+isTokemon(geanymon,normal,water,470,60,80).
+isTokemon(logkomon,normal,hmif,510,50,80).
+isTokemon(remon,normal,hme,790,60,90).
+isTokemon(radarmon,normal,signum,800,40,60).
 
-jenis(catamon,normal).
-jenis(dogmon,normal).
-jenis(birdmon,normal).
-jenis(mousemon,normal).
-jenis(garamon,legendary).
-jenis(daemon,legendary).
-jenis(kumon,legendary).
-jenis(doraemon,legendary).
+isTokemon(garamon,legendary,water,2102,120,200).
+isTokemon(daemon,legendary,fire,1011,200,400).
+isTokemon(kumon,legendary,fire,1234,123,456).
+isTokemon(doraemon,legendary,leaves,950,300,500).
+/*
+generatePeluang(X,P) :-
+    X == normal,
+    random(60,80,P).
+generatePeluang(X,P) :-
+    X == legendary,
+    random(10,40,P).  
+peluangEncounter(P) :- 
+    random(
+*/
+musuhnyaApa :- 
+    random(0,PanjangList,X),
+    ambil(ListMusuh,X,Tokemon),
+    isTokemon(Tokemon,Jenis,Tipe,HP,Attack,Sp_attack),
+    asserta(musuh(0,Tokemon,Jenis,Tipe,HP,Attack,Sp_attack))
+/*
+peluangRun(P) :- 
+*/
+ambil([],0,'') :- !.
+ambil([X|_],0,X) :- !.
+ambil([_|Tail],N,X) :- N1 is N-1, ambil(Tail, N1, X), !.
 
-tipe(catamon,water).
-tipe(dogmon,fire).
-tipe(birdmon,leaves).
-tipe(mousemon,leaves).
-tipe(garamon,fire).
-tipe(daemon,fire).
-tipe(kumon,water).
-tipe(doraemon,leaves).
-
-health(catamon,).
-health(dogmon,).
-health(birdmon,).
-health(mousemon,).
-health(garamon,).
-health(daemon,).
-health(kumon,).
-health(doraemon,).
-
-n_attack(catamon,).
-n_attack(dogmon,).
-n_attack(birdmon,).
-n_attack(mousemon,).
-n_attack(garamon,).
-n_attack(daemon,).
-n_attack(kumon,).
-n_attack(doraemon,).
-
-sp_attack(catamon,).
-sp_attack(dogmon,).
-sp_attack(birdmon,).
-sp_attack(mousemon,).
-sp_attack(garamon,).
-sp_attack(daemon,).
-sp_attack(kumon,).
-sp_attack(doraemon,).
+/*ketemu musuh, runnya berhasil apa engga, ketika ketemu musuhnya apa*/
