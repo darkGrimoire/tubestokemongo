dd07ba41879714ddc3a4af2bca60fd7c9775fbde
donbasta <35418377+donbasta@users.noreply.github.com>
2019-11-11 15:50:26 +0700

    adding player status

--- player.pl
+++ player.pl
@@ -1,6 +1,4 @@
 
-:- include('peta.pl').
-
 %contoh data
 
 %--------------------------------------------------------------------------
@@ -9,17 +7,14 @@
 
 %inisialisasi fakta player, masukin ke init_player
 
-init_player :-
-    asserta(tokemon(catamon,normal,water,1000,15,45)),
-    asserta(pos(5,6)),
-	maxTokemon(6).
+maxTokemon(6).
 
 
 %cari banyak tokemon yang lagi dipunya
 
 cekBanyakTokemon(banyak) :-
 	findall(B, tokemon(B,_,_,_,_,_), ListTokemon),
-	length(ListTokemon, banyak).
+	length(ListTokemon, banyak),
 
 
 %Nambahin tokemon, cek dulu banyak tokemon di database
@@ -37,7 +32,7 @@ addTokemon(Tokemon, Type, Elemental, HP, Atk, SpAtk) :-
 
 %gabisa jalan ke atas
 w :-
-	pos(_,Y),
+	pos(X,Y),
 	tinggipeta(Z),
 	Y =:= Z, 
 	write('Jalan anda menuju utara terhalang tembok besar!!'), nl, !.
@@ -51,7 +46,7 @@ w :-
 	asserta(pos(X,YNew)),!.
 
 s :-
-	pos(_,Y),
+	pos(X,Y),
 	Y =:= 1, 
 	write('Jalan anda menuju selatan terhalang tembok besar!!'), nl, !.
 
@@ -62,7 +57,8 @@ s :-
 	asserta(pos(X,YNew)),!.
 
 a :-
-	pos(X,_),
+	pos(X,Y),
+	lebarpeta(Z),
 	X =:= 1, 
 	write('Jalan anda menuju barat terhalang tembok besar!!'), nl, !.
 
@@ -73,7 +69,7 @@ a :-
 	asserta(pos(XNew,Y)),!.
 
 d :-
-	pos(X,_),
+	pos(X,Y),
 	lebarpeta(Z),
 	X =:= Z, 
 	write('Jalan anda menuju timur terhalang tembok besar!!'), nl, !.
@@ -84,12 +80,3 @@ d :-
 	X < Z,
 	XNew is X+1,
 	asserta(pos(XNew,Y)),!.
-
-
-
-%status
-
-
-
-
-
