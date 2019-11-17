d4ff4f15aee55e6226e12cdbf63fde7d54ed6030
annisarahim <53399300+annisarahim@users.noreply.github.com>
2019-11-17 22:10:04 +0700

    leveluph

--- tokemon.pl
+++ tokemon.pl
@@ -1,55 +1,70 @@
 :-dynamic(tokemon/11).  /*tokemon(nama)*/
 
-/*isTokemon(nama,jenis,tipe,hp,nama_attack,damage_attack,nama_sp_attack,damage_sp_attack,level,currExp,neededExp) */
-isTokemon(catamon,normal,water,HP,cakar_aja,A,cakar_banget,SA,1,0,100) :-
-    random(470,500,HP),
-    random(50,60,A),
-    random(70,80,SA).
-    
-isTokemon(defrog,normal,water,HP,kwok,A,frogyou,SA,1,0,100) :-
-    random(470,500,HP),
-    random(50,60,A),
-    random(70,80,SA).
+/*isTokemon(nama,jenis,tipe,hp,maxhp,nama_attack,damage_attack,nama_sp_attack,damage_sp_attack,level,currExp,neededExp) */
 
-isTokemon(anjingmon,normal,fire,HP,gukguk,A,anjingsia,SA,1,0,100) :-
-    random(450,480,HP),
-    random(55,65,A),
+isTokemon(catamon,normal,water,HP,5000,cakar_aja,A,cakar_banget,SA,1,0,1000) :-
+    random(4700,5000,HP),
+    random(500,600,A),
+    random(700,800,SA).
+   
+isTokemon(defrog,normal,water,HP,5000,kwok,A,frogyou,SA,1,0,1000) :-
+    random(4700,5000,HP),
+    random(500,600,A),
+    random(700,800,SA).
+/*    ///////////////////////////////////
+isTokemon(defrog,normal,water,HP,5500,kwok,A,frogyou,SA,2,100,200) :-
+    random(470,500,HP),
+    random(50,60,A),
+    random(70,80,SA). 
+isTokemon(catamon,normal,water,HP,550,cakar_aja,A,cakar_banget,SA,2,100,200) :-
+    random(470,500,HP),
+    random(50,60,A),
     random(70,80,SA).
     
-isTokemon(nagamon,normal,fire,HP,sembursaja,A,duarr,SA,1,0,100) :-
-    random(450,480,HP),
-    random(55,65,A),
+isTokemon(catamon,normal,water,HP,600,cakar_aja,A,cakar_banget,SA,3,200,300) :-
+    random(470,500,HP),
+    random(50,60,A),
     random(70,80,SA).
+////////////////////////////////////      */
+isTokemon(anjingmon,normal,fire,HP,6000,gukguk,A,anjingsia,SA,1,0,1000) :-
+    random(4500,4800,HP),
+    random(550,650,A),
+    random(700,800,SA).
+    
+isTokemon(nagamon,normal,fire,HP,sembursaja,A,duarr,SA,1,0,1000) :-
+    random(4500,4800,HP),
+    random(550,650,A),
+    random(700,800,SA).
 
-isTokemon(birdmon,normal,leaves,HP,cuitcuit,A,ciutciut,SA,1,0,100) :-
-    random(400,420,HP),
-    random(65,75,A),
-    random(85,95,SA).
+isTokemon(birdmon,normal,leaves,HP,cuitcuit,A,ciutciut,SA,1,0,1000) :-
+    random(4000,4200,HP),
+    random(650,750,A),
+    random(850,950,SA).
     
-isTokemon(tikusmon,normal,leaves,HP,puptikus,A,gigit_nih,1,0,100) :-
-    random(400,420,HP),
-    random(65,75,A),
-    random(85,95,SA).
+isTokemon(tikusmon,normal,leaves,HP,puptikus,A,gigit_nih,1,0,1000) :-
+    random(4000,4200,HP),
+    random(650,750,A),
+    random(850,950,SA).
 
-isTokemon(kodingmon,normal,hmif,HP,sublime,A,vscode,SA,1,0,100) :-
-    random(470,500,HP),
-    random(50,60,A),
-    random(70,80,SA).
+isTokemon(kodingmon,normal,hmif,HP,sublime,A,vscode,SA,1,0,1000) :-
+    random(4700,5000,HP),
+    random(500,600,A),
+    random(700,800,SA).
     
-isTokemon(nyetrumon,normal,hme,HP,kapasitor,A,induktor,SA,1,0,100) :-
-    random(470,500,HP),
-    random(50,60,A),
-    random(70,80,SA).
+isTokemon(nyetrumon,normal,hme,HP,kapasitor,A,induktor,SA,1,0,1000) :-
+    random(4700,5000,HP),
+    random(500,600,A),
+    random(700,800,SA).
     
-isTokemon(radarmon,normal,signum,HP,laprak,A,radiasi,SA,1,0,100) :-
-    random(470,500,HP),
-    random(50,60,A),
-    random(70,80,SA).
+isTokemon(radarmon,normal,signum,HP,laprak,A,radiasi,SA,1,0,1000) :-
+    random(4700,5000,HP),
+    random(500,600,A),
+    random(700,800,SA).
  
-isTokemon(garamon,legendary,water,2102,salt,120,saltbae,200,1,0,1000).
-isTokemon(daemon,legendary,hmif,1011,konsekuensi,200,pencoretan,400,1,0,1000). 
-isTokemon(kumon,legendary,fire,1234,english,123,math,456,1,0,1000).
-isTokemon(doraemon,legendary,leaves,950,baling_bambu,300,time_machine,500,1,0,1000).
+isTokemon(garamon,legendary,water,21020,salt,1200,saltbae,2000,1,0,10000).
+isTokemon(daemon,legendary,hmif,10110,konsekuensi,2000,pencoretan,4000,1,0,10000). 
+isTokemon(kumon,legendary,fire,12345,english,1234,math,2345,1,0,10000).
+isTokemon(doraemon,legendary,leaves,9500,baling_bambu,3000,time_machine,5000,1,0,10000).
 
 apakahBisaLevelUp(Bisa) :-
     findall(X, (inventory(X,_,_,_,_,_,_,_,Level,CurrExp,NeededExp), Level<3, CurrExp>=NeededExp), ListNonMaxLevel),
@@ -62,9 +77,19 @@ apakahBisaLevelUp(Bisa) :-
     ),!.
     
 levelUp(Tokemon) :-
-    retract(inventory(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp)),
-    asserta(inventory(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack+10,Nama_sp,Damage_sp+10,CurrExp,NeededExp+1000)),
-    write('Congratzszszs!!! Your '), write(Tokemon), write(' is now on level'), write(level+1), write('!'), nl, !.
+    inventory(Tokemon,Jenis,Tipe,HP,MaxHP,NamaAtk,DamageAtk,NamaSp,DamageSp,Level,CurrExp,NeededExp),
+    NewLevel is Level+1,
+    NewNeededExp is NeededExp+(NeededExp*1.2),
+    BatasBawah is 0.1*MaxHP,
+    BatasAtas is 0.3*MaxHP,
+    random(BatasBawah, BatasAtas, PlusHP),
+    NewMaxHP is floor(MaxHP+PlusHP),
+    NewHP is floor(HP+(PlusHP*0.75)),
+    NewDamageAtk is (Damage+(0.1*PlusHP)),
+    NewDamageSp is (Sp+(0.1*PlusHP)),
+    retract(inventory(Tokemon,Jenis,Tipe,HP,MaxHP,NamaAtk,DamageAtk,NamaSp,DamageSp,Level,CurrExp,NeededExp)),
+    asserta(inventory(Tokemon,Jenis,Tipe,NewHP,NewMaxHP,NamaAtk,NewDamageAtk,NamaSp,NewDamageSp,NewLevel,CurrExp,NewNededExp)),
+    write('Congratzszszs!!! Your '), write(Tokemon), write(' is now on level '), write(NewLevel), write('!'), nl, !.
     
 generateEncounter(Encounter) :-
     random(1,10,X),
@@ -89,7 +114,7 @@ ambilMusuhLegendary :-
     findall(X, (musuh(X,Tipe,_,_,_,_,_,_), Tipe == legendary), ListMusuhLegendary),
     length(ListMusuh, Panjang),
     random(0,Panjang,HasilRandom),
-    ambil(ListMusuhLegendary,HasilRandom,Tokemon),
+    searchIdx(ListMusuhLegendary,HasilRandom,Tokemon),
     isTokemon(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp),
     musuh(_,Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp),
     asserta(curMusuh(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp)),
@@ -101,7 +126,7 @@ ambilMusuhNormal :-
     findall(X, (musuh(X,Tipe,_,_,_,_,_,_), Tipe == normal), ListMusuhNormal),
     length(ListMusuh, Panjang),
     random(0,Panjang,HasilRandom),
-    ambil(ListMusuhNormal,HasilRandom,Tokemon),
+    searchIdx(ListMusuhNormal,HasilRandom,Tokemon),
     isTokemon(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp),
     musuh(_,Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp),
     asserta(curMusuh(Tokemon,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp)),
@@ -113,22 +138,19 @@ generatePeluangRun(X) :-
     random (1,10,X),
     X mod 4 =:= 0 ->
     (
-        X = berhasil,
+        X = berhasil,!
     );(
-        X = gagal,
+        X = gagal,!
     )!.
     
 init_musuh(0) :- !.
 init_musuh(Banyak) :-
     findall(X,isTokemon(X,_,_,_,_,_,_,_),ListMusuh),
-    ambil(ListMusuh,Banyak,TokemonMusuh),
+    searchIdx(ListMusuh,Banyak,TokemonMusuh),
     isTokemon(TokemonMusuh,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp),
     asserta(musuh(Banyak,Nama,Jenis,Tipe,HP,Nama_attack,Damage_attack,Nama_sp,Damage_sp,Level,CurrExp,NeededExp)),
     BanyakBaru is Banyak-1,
     initMusuh(BanyakBaru),!.
     
-ambil([],0,'') :- !.
-ambil([X|_],0,X) :- !.
-ambil([_|Tail],N,X) :- N1 is N-1, ambil(Tail, N1, X), !.
 
 /*ketemu musuh, runnya berhasil apa engga, ketika ketemu musuhnya apa*/
