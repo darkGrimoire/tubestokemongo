83476543269dca42bb0fc446e775ea04c7f61e0f
donbasta <faddilafarras@gmail.com>
2019-11-18 01:05:22 +0700

    something

--- riset.pl
+++ riset.pl
@@ -1,3 +1,5 @@
+
+/*
 :- dynamic(test/3).
 :- asserta(test(1000,2,500)),
     test(NeededExp, Level, HP),
@@ -9,3 +11,20 @@
     NewHP2 is floor(NewHP+HP),
     retract(test(_,_,_)),
     asserta(test(NewNeededExp,NewLevel,NewHP2)).
+*/
+
+%test :- retractall(inventory), retractall(isGym), retractall(pos).
+
+%inventory(catamon,normal,water,4800,5000,cakar_aja,500,cakar_banget,750,1,0,1000).
+%inventory(garamon,legendary,water,21020,salt,1200,saltbae,2000,1,0,10000).
+
+%pos(1,2).
+
+%isGym(1,2).
+
+try :- N is 1,
+	write(N),
+	N1 is N+1,
+	write(N1),
+	N is N1+1,
+	write(N).
