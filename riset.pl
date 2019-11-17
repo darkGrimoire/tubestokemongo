b0ba508dd5c56a388049140e48e3e12038f24a38
Faris-kun <faris.ekananda20@gmail.com>
2019-11-17 21:26:28 +0700

    riset keanehan arithmetic prolog

--- riset.pl
+++ riset.pl
@@ -1,35 +1,11 @@
-% Tokemon Database
-% tokemon(nama_pokemon,tipe,elemental,hp, atk, sp_atk, )
-isTokemon(catamon,normal,water,1000,15,45).
-isTokemon(dogmon,normal,fire,2500,80,350).
-isTokemon(garamon,legendary,fire,7500,250,800).
-isTokemon(daemon,legendary,fire,9999,500,2500).
-
-% init player:l
-init_player :-
-    asserta(tokemon(catamon,normal,water,1000,15,45)),
-    asserta(pos(5,6)).
-
-init_musuh :-
-    findall(T,isTokemon(T,_,_,_,_,_), ListTokemon),
-    length(ListTokemon,PanjangList),
-    random(0,PanjangList, NoTokemon),
-    ambil(ListTokemon, NoTokemon, Tokemon),
-    isTokemon(Tokemon, Type, Elemental, HP, Atk, SpAtk),
-    generatePeluang(Type, Peluang),
-    asserta(musuh(0,Tokemon, Type, Elemental, HP, Atk, SpAtk, Peluang)).
-
-
-
-generatePeluang(X,P) :-
-    X == normal,
-    random(60,80,P).
-generatePeluang(X,P) :-
-    X == legendary,
-    random(10,40,P).
-
-
-% Utils
-ambil([],0,'') :- !.
-ambil([X|_],0,X) :- !.
-ambil([_|Tail],N,X) :- N1 is N-1, ambil(Tail, N1, X), !.
+:- dynamic(test/3).
+:- asserta(test(1000,2,500)),
+    test(NeededExp, Level, HP),
+    NewLevel is Level+1,
+    NewNeededExp is NeededExp+((NewLevel)*1000),
+    LowBound is 0.1*HP,
+    HighBound is 0.3*HP,
+    random(LowBound, HighBound, NewHP),
+    NewHP2 is floor(NewHP+HP),
+    retract(test(_,_,_)),
+    asserta(test(NewNeededExp,NewLevel,NewHP2)).
